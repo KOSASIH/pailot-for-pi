c97d0aceec6f5edc2b0b2092182604aef1936184
KOSASIH <kosasihg88@gmail.com>
2024-12-03 12:02:17 +0700

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This application is built using React, Redux, and modern web technologies.</p>
+    </div>
+  );
+};
+
+export default About;
